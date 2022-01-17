1461e3d3e9a31d38b0f4cf3a3729d05c24dbca47
Chandana B R <Chandana.BR@trivium-esolutions.com>
2022-01-17 14:56:42 +0530

    Final Changes

---
+++ LibraryManagement/src/main/java/com/example/demo/Book.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="library")
+public class Book {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String title;
+	private String description;
+	private int publishedYear;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPublishedYear() {
+		return publishedYear;
+	}
+	public void setPublishedYear(int publishedYear) {
+		this.publishedYear = publishedYear;
+	}
+	
+	
+}
